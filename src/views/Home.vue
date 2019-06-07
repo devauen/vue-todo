edc674d1c2da7823c84fc1b90f5d79ec6323317d
Marcin Filipowski <proartz.devon@gmail.com>
2019-06-07 15:57:56 +0200

    Testing colors and typography

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus nobis veniam itaque ratione labore modi tenetur, officia aliquam quisquam veritatis ad quae dolorem odio voluptates unde laborum enim dolores.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi placeat minima recusandae error, maxime earum a laboriosam! Consequuntur id nobis magni amet expedita ratione non quos enim earum! Alias, quibusdam.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
