8df745b8b625b393eda5757e0b32f8829f742211
Marcin Filipowski <proartz.devon@gmail.com>
2019-06-07 16:23:40 +0200

    Buttons and Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus nobis veniam itaque ratione labore modi tenetur, officia aliquam quisquam veritatis ad quae dolorem odio voluptates unde laborum enim dolores.</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi placeat minima recusandae error, maxime earum a laboriosam! Consequuntur id nobis magni amet expedita ratione non quos enim earum! Alias, quibusdam.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subheading font-weight-bold">This is a subheading</p>
-    <p class="caption">This is a caption</p>
+    <v-btn class="pink white--test">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn flat color="pink">click me</v-btn>
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right>email</v-icon>
+    </v-btn>
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
   </div>
 </template>
 
