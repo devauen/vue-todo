7bc39dbf83036c9aef5b4bbb78a79481d081b59b
Marcin Filipowski <proartz.devon@gmail.com>
2019-06-08 19:08:12 +0200

    Sorting Projects

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,17 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+
+      <v-layout row class="mb-3">
+        <v-btn small flat color="grey" @click="sortBy('title')">
+          <v-icon left small>folder</v-icon>
+          <span class="caption text-lowercase">by project name</span>
+        </v-btn>
+        <v-btn small flat color="grey" @click="sortBy('person')">
+          <v-icon left small>person</v-icon>
+          <span class="caption text-lowercase">by person</span>
+        </v-btn>
+      </v-layout>
       
       <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
@@ -43,7 +54,12 @@
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         ]
       }
-    }
+    },
+    methods: {
+        sortBy(prop){
+          this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
+        }
+      }
   }
 </script>
 
