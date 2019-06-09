5481fe9010ecec0e05bf775fb7b71aeb4a8ab96f
Marcin Filipowski <proartz.devon@gmail.com>
2019-06-09 10:35:45 +0200

    Snackbars

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -54,6 +54,7 @@ export default {
                 db.collection('projects').add(project).then(() => {
                     this.loading = false;
                     this.dialog = false;
+                    this.$emit('projectAdded');
                 })
             }
         }
